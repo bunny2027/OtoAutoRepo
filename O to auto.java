a9391dc3d316fcd225bacc33a41a87fa776d3183
Harank-sucks-at-coding <haran.k@prhsrobotics.com>
2023-08-12 18:05:19 -0400

    update 3

--- O to auto.java
+++ O to auto.java
@@ -4,11 +4,9 @@
 
 package frc.robot;
 
-import com.ctre.phoenix.motorcontrol.TalonFXFeedbackDevice;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
-
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
